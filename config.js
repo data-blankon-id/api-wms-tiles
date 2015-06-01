03533223f7a35e31bc0decc2e62bf7570ed7ce6c
Dhi Aurrahman <diorahman@rockybars.com>
2015-06-01 07:03:48 +0700

    Using the rest service

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var SECOND = 1000;
 module.exports = {
-  rootUrl: 'http://portal.ina-sdi.or.id/arcgis/services/IGD/BatasAdministrasi_NKRI/MapServer/WmsServer',
+  rootUrl: 'http://portal.ina-sdi.or.id/arcgis/rest/services/IGD/BatasWilayah_NKRI/MapServer',
   cacheExpiresIn: 30 * 24 * 60 * 60 * SECOND,
   dir: __dirname + '/wms-tiles'
 };
